39fb9b745c4786bdac8718c9777a7528ab3a82b8
Cameron Cashwell <cdevconsulting@protonmail.com>
2018-06-02 17:26:17 -0400

    update

--- cv/move.js
+++ cv/move.js
@@ -8,17 +8,16 @@ function Toggle() {
    element.classList.toggle("opentoggle");
 }
 function Avatar(){ 
+    $("#coin").toggleClass("flip");    
     var Coin = document.getElementById("coin");
     var degrees = 0;
-//    body.onload = function() {
-      degrees += 1800;
-      console.log(degrees)
-      Coin.style.webkitTransform = "rotateY(" + degrees + "deg)";
-      Coin.style.MozTransform = "rotateY(" + degrees + "deg)";
-      Coin.style.msTransform = "rotateY(" + degrees + "deg)";
-      Coin.style.OTransform = "rotateY(" + degrees + "deg)";
-      Coin.style.transform = "rotateY(" + degrees + "deg)";
-//    }
+    degrees += 1800;
+    console.log(degrees)
+    Coin.style.webkitTransform = "rotateY(" + degrees + "deg)";
+    Coin.style.MozTransform = "rotateY(" + degrees + "deg)";
+    Coin.style.msTransform = "rotateY(" + degrees + "deg)";
+    Coin.style.OTransform = "rotateY(" + degrees + "deg)";
+    Coin.style.transform = "rotateY(" + degrees + "deg)";
 }
     
 setInterval(function(){ 
--- js/app.js
+++ js/app.js
@@ -7,15 +7,50 @@ var current = window.location.href;
 if(current == homepage) {
     setTimeout(function () {
         window.location.href = "http://www.thedevtip.com/nav/"; 
-    }, 2000);   
+    }, 9400);   
 } 
 else if (current == orpage){ 
     setTimeout(function () {
         window.location.href = "http://www.thedevtip.com/nav/"; 
-    }, 2000);   
+    }, 9400);   
 } else {
-//    block of code to be executed if the condition1 is false and condition2 is false
+
 }
+    
+function ShowLocalDate(){
+    var dNow = new Date();
+    var localdate= (dNow.getMonth()+1) + '/' + dNow.getDate() + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
+    $('.date').html(localdate)
+}
+ShowLocalDate();    
+    
+setTimeout(function(){ 
+$('#one').addClass('show')
+}, 400);
+
+setTimeout(function(){ 
+$('#two').addClass('show')
+}, 1200);
+
+setTimeout(function(){ 
+$('#three').addClass('show')
+}, 3000);
+
+setTimeout(function(){ 
+   $('#three').addClass('css-typing') 
+}, 3000);
+
+setTimeout(function(){ 
+$('#four').addClass('show')
+}, 4800);
+
+setTimeout(function(){ 
+$('#five').addClass('show')
+}, 6600);
+
+setTimeout(function(){ 
+  $('#five').addClass('css-typing') 
+}, 6600);    
     
 $.get("http://ipinfo.io", function (response) {
      var coords = response.loc;
